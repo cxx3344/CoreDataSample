097931d345092fdd0f464762b81aa86a6f47c658
cxx3344 <zyy1314666@gmail.com>
2015-01-09 14:49:36 +0800

    添加项目

---
+++ CoreDataSample/CoreDataSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataSample
+//
+//  Created by szyl on 15/1/9.
+//  Copyright (c) 2015年 Ideal.sh.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreDataSample/CoreDataSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreDataSample
+//
+//  Created by szyl on 15/1/9.
+//  Copyright (c) 2015年 Ideal.sh.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
